b40b809729c6125d0490f2737898af79baa57e86
John-David Deubl <print@strukturart.com>
2020-09-09 14:52:47 +0200

    prettier

--- application/assets/js/helper.js
+++ application/assets/js/helper.js
@@ -1,90 +1,88 @@
 function notify(param_title, param_text, param_silent, requireInteraction) {
-    var options = {
-        body: param_text,
-        silent: param_silent,
-        requireInteraction: requireInteraction,
-    };
+  var options = {
+    body: param_text,
+    silent: param_silent,
+    requireInteraction: requireInteraction,
+  };
 
-    var action = {
-        actions: [{
-            action: "archive",
-            title: "Archive",
-        }, ],
-    };
+  var action = {
+    actions: [
+      {
+        action: "archive",
+        title: "Archive",
+      },
+    ],
+  };
 
-    // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-        alert("This browser does not support desktop notification");
-    }
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
 
-    // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
-        // If it's okay let's create a notification
-        var notification = new Notification(param_title, options);
-    }
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(param_title, options);
+  }
 
-    // Otherwise, we need to ask the user for permission
-    else if (Notification.permission !== "denied") {
-        Notification.requestPermission().then(function(permission) {
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-                var notification = new Notification(param_title, options, action);
-            }
-        });
-    }
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(param_title, options, action);
+      }
+    });
+  }
 }
 
 //silent notification
 function toaster(text, time) {
-    $("div#toast").html(text);
+  $("div#toast").html(text);
 
-    $("div#toast").animate({ top: "0px" }, 1000, "linear", function() {
-        $("div#toast").delay(time).animate({ top: "-110vh" }, 1000);
-    });
+  $("div#toast").animate({ top: "0px" }, 1000, "linear", function () {
+    $("div#toast").delay(time).animate({ top: "-110vh" }, 1000);
+  });
 }
-
 
 //bottom bar
 function bottom_bar(left, center, right) {
+  $("div#bottom-bar div#button-left").text(left);
+  $("div#bottom-bar div#button-center").text(center);
+  $("div#bottom-bar div#button-right").text(right);
 
-    $("div#bottom-bar div#button-left").text(left)
-    $("div#bottom-bar div#button-center").text(center)
-    $("div#bottom-bar div#button-right").text(right)
-
-    if (left == "" && center == "" && right == "") {
-        $("div#bottom-bar div#button-left").css("display", "none")
-    } else {
-        $("div#bottom-bar div#button-left").css("display", "block")
-
-    }
+  if (left == "" && center == "" && right == "") {
+    $("div#bottom-bar div#button-left").css("display", "none");
+  } else {
+    $("div#bottom-bar div#button-left").css("display", "block");
+  }
 }
-
-
 
 //check if internet connection
 function check_iconnection() {
-    if (navigator.onLine) {} else {
-        toaster("No Internet connection");
-    }
+  if (navigator.onLine) {
+  } else {
+    toaster("No Internet connection");
+  }
 }
 
 //wake up screen
 function screenWakeLock(param1) {
-    if (param1 == "lock") {
-        lock = window.navigator.requestWakeLock("screen");
+  if (param1 == "lock") {
+    lock = window.navigator.requestWakeLock("screen");
 
-        lock.onsuccess = function() {
-            toaster("screen-lock", 10000);
-        };
+    lock.onsuccess = function () {
+      toaster("screen-lock", 10000);
+    };
 
-        lock.onerror = function() {
-            alert("An error occurred: " + this.error.name);
-        };
+    lock.onerror = function () {
+      alert("An error occurred: " + this.error.name);
+    };
+  }
+
+  if (param1 == "unlock") {
+    if (lock.topic == "screen") {
+      lock.unlock();
     }
-
-    if (param1 == "unlock") {
-        if (lock.topic == "screen") {
-            lock.unlock();
-        }
-    }
+  }
 }
